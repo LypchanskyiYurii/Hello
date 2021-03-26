583df421ea56544680c29ade0e9dedea4dfba60b
YuriiLipchanskiy <YuriiLipchanskiy>
2021-03-26 23:52:17 +0200

    Add SayHi.java

---
+++ src/ua/yrix/sayhi/SayHi.java
@@ -0,0 +1,9 @@
+package ua.yrix.sayhi;
+
+public class SayHi {
+    public static void main(String[] args) {
+        System.out.println("_______________");
+        System.out.println("Hello world");
+        System.out.println("_______________");
+    }
+}
