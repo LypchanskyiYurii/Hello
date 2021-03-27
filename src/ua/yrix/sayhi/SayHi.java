0d9cf97bde7feb3e6b11206897ff5ab386d13d69
YuriiLipchanskiy <YuriiLipchanskiy>
2021-03-27 23:56:53 +0200

    Add input with consol)))

--- src/ua/yrix/sayhi/SayHi.java
+++ src/ua/yrix/sayhi/SayHi.java
@@ -1,9 +1,22 @@
 package ua.yrix.sayhi;
 
+import java.util.Scanner;
+
 public class SayHi {
     public static void main(String[] args) {
         System.out.println("_______________");
         System.out.println("Hello world");
         System.out.println("_______________");
+
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Введіть число:");
+        int number = scaner.nextInt();
+        while (true) {
+            if (number % 2 == 0) {
+                System.out.println("Ви ввели парне число");
+                break;
+            } else System.out.println("Ви ввели не парне число");
+            break;
+        }
     }
 }
